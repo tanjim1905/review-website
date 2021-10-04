537a32e1e144010bb871cda4c7d832be0f3a5314
tanjim1905 <tanjimahmed1905@gmail.com>
2021-10-04 23:31:01 +0600

    added fav icon

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,6 +14,13 @@ const Home = () => {
         <div>
             <Header></Header>
             <Container>
+            <Row>
+                <div className="col-12 col-md-10 col-lg-8 mx-auto">
+                    <h2 className="text-center">Our Courses</h2>
+
+                    <p className="text-center">we know that not only innovative technologies determine the quality of the product. A very important aspect in the production process is the contribution of our employees.</p>
+                </div>
+            </Row>
                 <Row xs={1} md={2} lg={4} className="g-4 my-5">
                 {
                     homeCourse.slice(0, 4).map((course) => <Course key={course.key} course={course}></Course>)
