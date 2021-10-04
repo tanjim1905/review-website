8aadbb04db86f949ee398610693483f2dca89480
tanjim1905 <tanjimahmed1905@gmail.com>
2021-10-04 23:52:45 +0600

    fix footer

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className="error pt-5" style={{minHeight: "80vh"}}>
             <h1 className="text-danger text-center mt-5">404! Page Not Found !!</h1>
         </div>
     );
